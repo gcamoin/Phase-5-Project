1c1317e9996a6f853c01fd368f969410c90ca3fe
Garrett <gcamoin5@gmail.com>
2024-02-06 15:21:43 -0500

    created UserContext file

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Login from "/home/gcamoin/phase-5-project/client/src/components/Login.js"
 import SignUp from "/home/gcamoin/phase-5-project/client/src/components/SignUp.js"
+import { UserContext } from "/home/gcamoin/phase-5-project/client/src/components/contexts/UserContext.js"
 
 function App() {
   return (
