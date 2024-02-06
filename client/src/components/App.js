dbd5aa560e782d6e47853f3dac05e25e305eb15a
Garrett <gcamoin5@gmail.com>
2024-02-06 16:34:02 -0500

    reverted changes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,34 +1,20 @@
 import React, {useState, useContext} from "react"
 import Login from "/home/gcamoin/phase-5-project/client/src/components/Login.js"
 import SignUp from "/home/gcamoin/phase-5-project/client/src/components/SignUp.js"
-import { UserContext } from "/home/gcamoin/phase-5-project/client/src/components/contexts/UserContext.js"
-import GenreContainer from "/home/gcamoin/phase-5-project/client/src/components/GenreContainer.js"
+
+
 
 function App() {
 
-  const {user, setUser} = useContext(UserContext)
+  return (
+    <div className="app">
 
-  if (user) {
-    return ( 
-    <div className="logged-in page">
-      
-      {GenreContainer}
-      
-       
-      
-    </div>
-    )
-  } else {
-    return (
-     <div className="login-page">
-      
-      <Login  /> 
-      <br></br> 
+      <Login />
+      <br></br>
       <SignUp />
-      </div>
-    )
-  }
 
+    </div>
+  )
   
 }
 
