36b8c8ae09510f9f47c9729eaff22db421a1f5d3
Garrett <gcamoin5@gmail.com>
2024-02-06 14:29:59 -0500

    added sessions controller

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react"
 import Login from "/home/gcamoin/phase-5-project/client/src/components/Login.js"
+import SignUp from "/home/gcamoin/phase-5-project/client/src/components/SignUp.js"
 
 function App() {
   return (
     <div className="App">
 
       <Login />
+      <br></br>
+      <SignUp />
       
     </div>
   );
