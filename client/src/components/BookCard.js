7d14f4b1afa1b67b4de8b78711057557620d636c
Garrett <gcamoin5@gmail.com>
2024-02-07 18:37:36 -0500

    changed seed data

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -1,11 +1,26 @@
 import {React, useState} from "react"
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
 
-function BookCard() {
+function BookCard({book}) {
 
+    const {id, title, author, image, reviews } = book
 
 
     return (
-        
+        <div className="book-card">
+            <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    component="img"
+                    alt="book image"
+                    height="500"
+                    image={image}
+                />
+                {title}
+
+            </Card>
+            
+        </div>
     )
 
 
--- client/src/components/GenreCard.js
+++ client/src/components/GenreCard.js
@@ -1,8 +1,17 @@
 import {React, useState} from "react"
 import {Typography} from '@mui/material';
+import BookCard from "/home/gcamoin/phase-5-project/client/src/components/BookCard.js"
 
 function GenreCard({genre}) {
     const {genre_id, name, books} = genre
+
+    const bookList = books?.map((book) => (
+        <BookCard 
+            key={book.id}
+            book={book}
+        
+        />
+    ))
 
    
     return (
@@ -10,7 +19,7 @@ function GenreCard({genre}) {
           
             
             <Typography sx={{ mt: 8, ml: 7, fontFamily: "cursive", textDecoration: "underline" }}  color='primary' variant="h4">{name}</Typography>
-                
+             {bookList}   
              
      
           
