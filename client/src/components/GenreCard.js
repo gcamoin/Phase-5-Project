46e6294dee584ef587a10028968589c78d4be541
Garrett <gcamoin5@gmail.com>
2024-02-09 16:06:44 -0500

    added image column to genres

--- client/src/components/GenreCard.js
+++ client/src/components/GenreCard.js
@@ -1,27 +1,51 @@
 import {React, useState} from "react"
-import {Typography} from '@mui/material';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography';
 import BookCard from "/home/gcamoin/phase-5-project/client/src/components/BookCard.js"
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
 
 function GenreCard({genre}) {
     const {genre_id, name, books} = genre
 
-    const bookList = books?.map((book) => (
-        <BookCard 
-            key={book.id}
-            book={book}
+    // const bookList = books?.map((book) => (
+    //     <BookCard 
+    //         key={book.id}
+    //         book={book}
         
-        />
-    ))
+    //     />
+    // ))
 
    
     return (
         <div className="genre-cards">
           
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+                sx={{ height: 140 }}
+                image="/static/images/cards/contemplative-reptile.jpg"
+                title={name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                {name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over 6,000
+                species, ranging across all continents except Antarctica
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Browse Books</Button>
             
-            <Typography sx={{ mt: 8, fontFamily: "fantasy", textDecoration: "underline" }}  color='primary' variant="h4" align="center">{name}</Typography>
-             {bookList}   
+            </CardActions>
+            </Card>
+            {/* <Typography sx={{ mt: 8, fontFamily: "fantasy", textDecoration: "underline" }}  color='primary' variant="h4" align="center">{name}</Typography>
+             {bookList}    */}
              
-     
+             {/* {bookList} */}
           
         </div>
     )
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_06_222549) do
+ActiveRecord::Schema.define(version: 2024_02_09_210525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2024_02_06_222549) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "reviews", force: :cascade do |t|
